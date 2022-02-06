e388a1e4671d79b87df9cc2a9e3c8e85879173c0
jey0623 <jey0623@naver.com>
2022-02-06 18:41:25 +0900

    로그인 추가

--- src/main/java/com/eunyeong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/eunyeong/book/springboot/service/posts/PostsService.java
@@ -8,9 +8,9 @@ import com.eunyeong.book.springboot.web.dto.PostsSaveRequestDto;
 import com.eunyeong.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.transaction.Transactional;
+
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,12 +33,20 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+    postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
